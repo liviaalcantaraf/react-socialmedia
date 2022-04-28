4ab74e7f9104292a21bd9b4f85d704a7ce0f6820
liviaalcantaraf <liviaalcantaraf@gmail.com>
2022-04-28 11:16:32 +0100

    Add Newpost

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
 import Feed from "./components/Feed";
 import Navbar from "./components/Navbar";
+import New from "./components/New";
 import Rightbar from "./components/Rightbar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <New />
+      </Box>
     </div>
   );
 }
